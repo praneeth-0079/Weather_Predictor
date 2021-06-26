2c22f7ceb78c3ae4fb716930c2dc163add90c3e2
praneeth <85471954+praneeth-0079@users.noreply.github.com>
2021-06-26 13:49:00 +0530

    Update bootcamp_project.py

--- bootcamp_project.py
+++ bootcamp_project.py
@@ -1,13 +1,13 @@
 import requests
 api='298c5dc93064a6f65f2124f2aec08b6f'
 location=input("enter location(city name):")
-URL="api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api
-link=requests.get(URL)
-data=link.json()
-temperature=((data['main']['temp'])-273.15)
-weather=data['weather'][0]['description']
-humidity=data['main']['humidity']
-windspeed=data=['wind']['speed']
+API_link="api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api
+Report=requests.get(API_link)
+About=Report.json()
+temperature=((About['main']['temp'])-273.15)
+weather=About['weather'][0]['description']
+humidity=About['main']['humidity']
+windspeed=About['wind']['speed']
 with open('weather.txt','w') as f:
     f.write(temperature)
     f.write(weather)
