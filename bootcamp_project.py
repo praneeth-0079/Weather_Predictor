6bc3db74df8d14073bcc57c40417bd7a112d000d
praneeth <85471954+praneeth-0079@users.noreply.github.com>
2021-06-27 15:06:39 +0530

    Update bootcamp_project.py

--- bootcamp_project.py
+++ bootcamp_project.py
@@ -1,16 +1,20 @@
 import requests
-api='298c5dc93064a6f65f2124f2aec08b6f'
-location=input("enter location(city name):")
-API_link="api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api
-Report=requests.get(API_link)
-About=Report.json()
-temperature=((About['main']['temp'])-273.15)
-weather=About['weather'][0]['description']
-humidity=About['main']['humidity']
-windspeed=About['wind']['speed']
+api = '298c5dc93064a6f65f2124f2aec08b6f'
+location = input("enter location(city name):")
+API_link = "api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api
+Report = requests.get(API_link)
+About = Report.json()
+tempe = ((About['main']['temp'])-273.15)
+Wthr = About['weather'][0]['description']
+Hmd = About['main']['humidity']
+WS = About['wind']['speed']
 with open('weather.txt','w') as f:
-    f.write(temperature)
-    f.write(weather)
-    f.write(humidity)
-    f.write(windspeed)
+    f.write("temperature in deg C:")
+    f.write(tempe)
+    f.write("weather:")
+    f.write(Wthr)
+    f.write("Humidity in %:")
+    f.write(Hmd)
+    f.write("wind speed in KMPH:")
+    f.write(WS)
 
